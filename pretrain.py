c3b9d182a8d5ec76b3955171e161e1fcee673f23
Abhishek Srikanth <abhisrik99@outlook.com>
2024-06-19 21:43:04 -0400

    feat: Add pretrain_model function to pretrain.py

---
+++ pretrain.py
@@ -0,0 +1,11 @@
+def pretrain_model(model_generator, tasks):
+
+    # Create a model
+    model = model_generator()
+
+    # Pretrain the model on all tasks
+    for i, task in enumerate(tasks):
+        print("task: ", i, " of ", len(tasks))
+        x_train, y_train = task.get_train_data()
+        model.fit(x_train, y_train, epochs=1, verbose=0)
+    return model
