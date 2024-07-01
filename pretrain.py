10161d508b0692f4d4983bb9e9360e81534988fa
Abhishek Srikanth <abhisrik99@outlook.com>
2024-06-30 23:39:18 -0400

    feat: Add verbose option to pretrain_model function

--- pretrain.py
+++ pretrain.py
@@ -1,11 +1,17 @@
-def pretrain_model(model_generator, tasks):
+from typing import Callable
+
+def pretrain_model(model_generator: Callable, tasks: list, gradient_steps: int=1000, verbose: int=0):
 
     # Create a model
     model = model_generator()
 
     # Pretrain the model on all tasks
     for i, task in enumerate(tasks):
-        print("task: ", i, " of ", len(tasks))
+
+        if verbose:
+            print("Task:", i+1, "/", len(tasks))
+
         x_train, y_train = task.get_train_data()
-        model.fit(x_train, y_train, epochs=1000, verbose=0)
+        model.fit(x_train, y_train, epochs=gradient_steps, verbose=verbose)
+        
     return model
